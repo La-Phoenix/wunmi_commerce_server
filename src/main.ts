70419dc25a9fc0330d8cbf31d9c73728f0082d85
La Phoenix <samuelokundalaiye@gmail.com>
2025-01-02 11:42:40 +0100

    Minute changes

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,6 @@ async function bootstrap() {
   
   const app = await NestFactory.create(AppModule);
 
-
-  // app.use(cookieParser());
-  // Enable CORS
   app.enableCors({
     origin: ['http://localhost:5173', 'https://la-phoenix.github.io', 'https://accounts.google.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
@@ -26,40 +23,4 @@ async function bootstrap() {
 }
 bootstrap();
 
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import * as dotenv from 'dotenv';
-// import * as cookieParser from 'cookie-parser';
-// import serverlessExpress from '@vendia/serverless-express';
-// import { APIGatewayProxyEvent, Context, Handler, Callback } from 'aws-lambda';
-
-// dotenv.config();
-
-// let server: Handler;
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   app.use(cookieParser());
-
-//   app.enableCors({
-//     origin: ['https://la-phoenix.github.io/wunmi_store', 'https://accounts.google.com'],
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//     credentials: true,
-//     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
-//     exposedHeaders: ['Content-Range', 'X-Content-Range'],
-//   });
-
-//   app.setGlobalPrefix('api/v1');
-//   await app.init();
-
-//   const expressApp = app.getHttpAdapter().getInstance();
-//   return serverlessExpress({ app: expressApp });
-// }
-
-// export const handler: Handler = async (event, context, callback) => {
-//   if (!server) {
-//     server = await bootstrap(); // Only bootstrap the server once
-//   }
-//   return server(event, context, callback);
-// };
 
