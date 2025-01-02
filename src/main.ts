ac05cfecbf5d8a42a119e6a0f06ebd39f14e4108
La Phoenix <samuelokundalaiye@gmail.com>
2025-01-02 10:40:55 +0100

    Solved cors issues for production

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,7 @@ async function bootstrap() {
   // app.use(cookieParser());
   // Enable CORS
   app.enableCors({
-    // origin: ['http://localhost:5173', 'https://la-phoenix.github.io/wunmi_store/', 'https://accounts.google.com'],
-    origin: '*', // Allow all origins
+    origin: ['http://localhost:5173', 'https://la-phoenix.github.io/wunmi_store', 'https://accounts.google.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
