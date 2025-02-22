cb981f81dada5e3f1072da0a1b8f931d6805fb11
La-Phoenix <samuelokundalaiye@gmail.com>
2025-02-22 08:45:59 +0100

    Modified cookie config

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -76,9 +76,9 @@ export class AuthController {
     const token = await this.authService.generateToken(user);
     // Redirect back to the frontend with the token in the URL
     res.cookie('token', token, {
-      httpOnly: false,          // Makes the cookie accessible to JavaScript
+      httpOnly: true,          // Makes the cookie accessible to JavaScript
       secure: this.isProduction,                  // Use HTTPS-only cookies in production
-      sameSite: 'none', // 'none' needed for cross-site in production
+      sameSite: 'lax', // 'none' needed for cross-site in production
       maxAge: 24 * 60 * 60 * 1000, // Set expiry time (e.g., 1 day)
       
     });
